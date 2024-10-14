724c78142c5cd1e316c91161304d977d2876aaac
adeeleye <adeleyejamaldeen6@gmail.com>
2024-10-14 15:39:46 +0100

    I just finished the project

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
-console.log("I am learning js objects, arrays and functions");
+// object
+const firstname = 'Jamal';
+const lastname = 'Adeleye';
+const age = '20';
+const isMarried = false;
+const address = 'Lagos';
+const skill1 = 'HTML';
+const skill2 = 'CSS';
+const skill3 = 'Javascipt';
+
+let person = {};
+
+console.log("this is an empty person project", person);
 
